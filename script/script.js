c3f9bb79ee2e49012878e350d18c1d5220df19f7
aidenjanzen <aidenw.janzen@outlook.com>
2023-11-15 08:31:04 -0800

    Cleaned Css

--- script/script.js
+++ script/script.js
@@ -1,6 +1,3 @@
-
-
-
 var countDownDate = new Date("Dec 4, 2023 19:25:00").getTime();
 
 var x = setInterval(function () {
@@ -23,7 +20,7 @@ var x = setInterval(function () {
         document.getElementById("text").innerHTML = "one month left, feels like no time at all!"
     }
     else if (days < 21 && days > 19){
-        document.getElementById("text").innerHTML = "three weeks left, she will come back to you "
+        document.getElementById("text").innerHTML = "three weeks left, she's coming back to you "
     }
     else if (days < 14 && days > 12){
         document.getElementById("text").innerHTML = "two weeks left, almost there! "
@@ -34,6 +31,15 @@ var x = setInterval(function () {
     else if (days < 5 && days > 4){
         document.getElementById("text").innerHTML = "five days left!! "
     }   
+    else if (days < 4 && days > 3){
+        document.getElementById("text").innerHTML = "four days left!! "
+    } 
+    else if (days < 3 && days > 2){
+        document.getElementById("text").innerHTML = "three days left!! "
+    } 
+    else if (days < 2 && days > 1){
+        document.getElementById("text").innerHTML = "two days left!! "
+    } 
     else if ((days < 1 && days >= 0) && (hours < 24 && hours >0)){
         document.getElementById("text").innerHTML = "one day left!!! "
     }   
